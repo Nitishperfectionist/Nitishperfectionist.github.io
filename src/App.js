14af02f794185176f42f29c3aa7289a247530914
Nitish kumar <nitishkumar2809@gmail.com>
2023-11-20 13:35:27 +0530

    new commit after modify

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
-import React from 'react';
+// import React from 'react';
 
+// import Navbar from './Component/Navbar';
+// import Home from './Pages/Home';
+// import About from './Pages/About';
+// import Skills from './Pages/Skills';
+// import Projects from './Pages/Projects';
+// import GitHubSubmission from './Pages/GitHubSubmission';
+// import Contacts from './Pages/Contacts';
+
+// function App() {
+//   return (
+//     <>
+//       <Home />
+//       <About/>
+//       <Skills/>
+//       <Projects/>
+//       <GitHubSubmission/>
+//       <Contacts/>
+//       <Navbar />
+//     </>
+//   );
+// }
+
+// export default App;
+import React, { useState } from 'react';
 import Navbar from './Component/Navbar';
 import Home from './Pages/Home';
 import About from './Pages/About';
@@ -7,17 +31,33 @@ import Skills from './Pages/Skills';
 import Projects from './Pages/Projects';
 import GitHubSubmission from './Pages/GitHubSubmission';
 import Contacts from './Pages/Contacts';
+import DrawerChakra from './Pages/DrawerChakra';
 
 function App() {
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!isMenuOpen);
+  };
+
+  const yourLinksArray = [
+    { id: 'home', title: 'Home', className: 'nav-link home' },
+    { id: 'about', title: 'About', className: 'nav-link about' },
+    { id: 'skills', title: 'Skills', className: 'nav-link skills' },
+    { id: 'projects', title: 'Projects', className: 'nav-link projects' },
+    { id: 'contact', title: 'Contacts', className: 'nav-link contact' },
+  ];
+
   return (
     <>
+      <Navbar closeMenu={toggleMenu} />
       <Home />
-      <About/>
-      <Skills/>
-      <Projects/>
-      <GitHubSubmission/>
-      <Contacts/>
-      <Navbar />
+      <About />
+      <Skills />
+      <Projects />
+      <GitHubSubmission />
+      <Contacts />
+      <DrawerChakra links={yourLinksArray} closeMenu={toggleMenu} />
     </>
   );
 }
--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -1,63 +1,146 @@
-import { Box, ChakraProvider, Flex, Spacer, theme, Button, Link as ResumeLink, useToast } from '@chakra-ui/react'
-import React from 'react'
-import { ColorModeSwitcher } from '../ColorModeSwitcher'
+// import { Box, ChakraProvider, Flex, Spacer, theme, Button, Link as ResumeLink, useToast } from '@chakra-ui/react'
+// import React from 'react'
+// import { ColorModeSwitcher } from '../ColorModeSwitcher'
+// import { BiDownload } from 'react-icons/bi';
+// import { Link } from "react-scroll";
+// import DrawerChakra from '../Pages/DrawerChakra';
+// import Resume from "../Essentials/Nitish-Kumar-Resume.pdf"
+
+
+// const Navbar = () => {
+//   const links = [
+//     { id: "home", title: "Home", className: "nav-link home" },
+//     { id: "about", title: "About", className: "nav-link about" },
+//     { id: "skills", title: "Skills", className: "nav-link skills" },
+//     { id: "projects", title: "Projects", className: "nav-link projects" },
+//     { id: "contact", title: "Contacts", className: "nav-link contact" },
+//   ]
+
+//   const toast = useToast()
+
+//   const handleDownload = () => {
+//     window.open('https://drive.google.com/file/d/17Jc7CHswy0og9uOVzqf1Un7HdSHmaLmi/view?usp=sharing', '_blank');
+//     toast({
+//       title: "Resume-downloading",
+//       status: 'success',
+//       duration: 5000,
+//       isClosable: true,
+//     })
+//   };
+
+//   return (
+//     <Flex id="nav-menu" bg={'#60a5fa'} w={'100%'} color={'white'} position={'fixed'} top={0} >
+//       <Box id="user-detail-name" fontSize={20} fontWeight={500} ml={[3, 10, 10]} cursor={'pointer'} >
+//         Nitish Kumar
+//       </Box>
+//       <Spacer />
+//       <Box my={'auto'} fontSize={20} display={['none', 'none', 'block']} cursor={'pointer'}  >
+//         {links.map((ele, i) => {
+//           return (
+//             <Link className={ele.className} key={i} to={ele.id} style={{ marginLeft: "30px" }} spy={true} smooth={true} offset={50} duration={500} ><a>{ele.title}</a></Link>
+//           )
+//         })}
+//         <Button target='_blank' id="resume-button-1" className="nav-link resume" fontSize={20} colorScheme='#60a5fa' leftIcon={<BiDownload />} ml={5} >
+//           <ResumeLink id="resume-link-1"
+//             rel="noreferrer"
+//             onClick={handleDownload}
+//             href={Resume}
+//             download={"Nitish-Kumar-Resume"}
+//           >RESUME
+//           </ResumeLink>
+//         </Button>
+//         <ChakraProvider theme={theme} >
+//           <ColorModeSwitcher justifySelf="flex-end" />
+//         </ChakraProvider>
+//       </Box>
+//       <Box display={['block', 'block', 'none']} fontSize={30} m={'auto'} mr={[1, 10, 10]} color={'white'}>
+//         <DrawerChakra links={links} />
+//       </Box>
+//     </Flex>
+//   )
+// }
+
+// export default Navbar
+// Navbar.js
+
+import React from 'react';
+import { Box, Flex, Spacer, Button, Link as ResumeLink, useToast } from '@chakra-ui/react';
 import { BiDownload } from 'react-icons/bi';
-import { Link } from "react-scroll";
+import { Link } from 'react-scroll';
 import DrawerChakra from '../Pages/DrawerChakra';
-import Resume from "../Essentials/Nitish-Kumar-Resume.pdf"
+import Resume from '../Essentials/Nitish-Kumar-Resume.pdf';
 
-
-const Navbar = () => {
+const Navbar = ({ closeMenu }) => {
   const links = [
-    { id: "home", title: "Home", className: "nav-link home" },
-    { id: "about", title: "About", className: "nav-link about" },
-    { id: "skills", title: "Skills", className: "nav-link skills" },
-    { id: "projects", title: "Projects", className: "nav-link projects" },
-    { id: "contact", title: "Contacts", className: "nav-link contact" },
-  ]
+    { id: 'home', title: 'Home', className: 'nav-link home' },
+    { id: 'about', title: 'About', className: 'nav-link about' },
+    { id: 'skills', title: 'Skills', className: 'nav-link skills' },
+    { id: 'projects', title: 'Projects', className: 'nav-link projects' },
+    { id: 'contact', title: 'Contacts', className: 'nav-link contact' },
+  ];
 
-  const toast = useToast()
+  const toast = useToast();
 
   const handleDownload = () => {
     window.open('https://drive.google.com/file/d/17Jc7CHswy0og9uOVzqf1Un7HdSHmaLmi/view?usp=sharing', '_blank');
     toast({
-      title: "Resume-downloading",
+      title: 'Resume-downloading',
       status: 'success',
       duration: 5000,
       isClosable: true,
-    })
+    });
   };
 
   return (
-    <Flex id="nav-menu" bg={'#60a5fa'} w={'100%'} color={'white'} position={'fixed'} top={0} >
-      <Box id="user-detail-name" fontSize={20} fontWeight={500} ml={[3, 10, 10]} cursor={'pointer'} >
+    <Flex id="nav-menu" bg={'#60a5fa'} w={'100%'} color={'white'} position={'fixed'} top={0}>
+      <Box id="user-detail-name" fontSize={20} fontWeight={500} ml={[3, 10, 10]} cursor={'pointer'}>
         Nitish Kumar
       </Box>
       <Spacer />
-      <Box my={'auto'} fontSize={20} display={['none', 'none', 'block']} cursor={'pointer'}  >
-        {links.map((ele, i) => {
-          return (
-            <Link className={ele.className} key={i} to={ele.id} style={{ marginLeft: "30px" }} spy={true} smooth={true} offset={50} duration={500} ><a>{ele.title}</a></Link>
-          )
-        })}
-        <Button target='_blank' id="resume-button-1" className="nav-link resume" fontSize={20} colorScheme='#60a5fa' leftIcon={<BiDownload />} ml={5} >
-          <ResumeLink id="resume-link-1"
+      <Box my={'auto'} fontSize={20} display={['none', 'none', 'block']} cursor={'pointer'}>
+        {links.map((ele, i) => (
+          <Link
+            className={ele.className}
+            key={i}
+            to={ele.id}
+            style={{ marginLeft: '30px' }}
+            spy={true}
+            smooth={true}
+            offset={50}
+            duration={500}
+            onClick={closeMenu} // Close menu on link click
+          >
+            <a>{ele.title}</a>
+          </Link>
+        ))}
+        <Button
+          target='_blank'
+          id="resume-button-1"
+          className="nav-link resume"
+          fontSize={20}
+          colorScheme='#60a5fa'
+          leftIcon={<BiDownload />}
+          ml={5}
+        >
+          <ResumeLink
+            id="resume-link-1"
             rel="noreferrer"
-            onClick={handleDownload}
+            onClick={() => {
+              handleDownload();
+              closeMenu();
+            }} // Close menu on resume click
             href={Resume}
-            download={"Nitish-Kumar-Resume"}
-          >RESUME
+            download={'Nitish-Kumar-Resume'}
+          >
+            RESUME
           </ResumeLink>
         </Button>
-        <ChakraProvider theme={theme} >
-          <ColorModeSwitcher justifySelf="flex-end" />
-        </ChakraProvider>
       </Box>
       <Box display={['block', 'block', 'none']} fontSize={30} m={'auto'} mr={[1, 10, 10]} color={'white'}>
-        <DrawerChakra links={links} />
+        <DrawerChakra links={links} closeMenu={closeMenu} />
       </Box>
     </Flex>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
